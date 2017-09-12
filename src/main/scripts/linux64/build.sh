08a332f0a3e5b8071cf8c40c47478dbf5b6e40dc
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2017-09-12 17:40:45 +0200

    [YIL-332] Ogg native files.

---
+++ src/main/scripts/linux64/build.sh
@@ -0,0 +1,17 @@
+cmake ../../c++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="../../../../target/classes/linux64" -G "Unix Makefiles"
+
+make install
+r1=$?
+
+make clean
+
+rm -R CMakeFiles
+rm CMakeCache.txt
+rm cmake_install.cmake
+rm Makefile
+rm install_manifest.txt
+rm -R include
+rm ogg.pc
+
+return $r1
+
---
+++ src/main/scripts/win32/build.sh
@@ -0,0 +1,16 @@
+cmake ../../c++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="../../../../target/classes/win32" -DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
+
+make install
+r1=$?
+
+make clean
+
+rm -R CMakeFiles
+rm CMakeCache.txt
+rm cmake_install.cmake
+rm Makefile
+rm install_manifest.txt
+rm -R include
+rm ogg.pc
+
+return $r1
